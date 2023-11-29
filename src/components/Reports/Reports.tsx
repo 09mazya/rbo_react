3e80d01268540370f729aa6e0f007ba7b94958b2
WhiteHanClean <whitehanclean@gmail.com>
2023-11-29 16:12:40 +0600

    add select not styles

--- src/components/Reports/Reports.tsx
+++ src/components/Reports/Reports.tsx
@@ -1,22 +1,37 @@
-import React, { useEffect } from "react";
+import React, { useState } from "react";
 import s from "./Reports.module.scss";
 import { observer } from "mobx-react-lite";
 import reportsStore from "../../store/ReportsStore";
 
 const Reports: React.FC = observer(() => {
+  const [selectedKey, setSelectedKey] = useState<string | null>(null);
   const { filteredReports } = reportsStore;
 
-  console.log(filteredReports.response);
+  const openBlock = (key: string) => {
+    setSelectedKey(key);
+  };
 
   return (
     <div className={s.reports}>
       <h2>Отчёты:</h2>
       {filteredReports.response ? (
-        Object.keys(filteredReports.response).map((key) => (
-          <li key={key}>{key}</li>
+        Object.keys(filteredReports.response).map((key: string) => (
+          <div key={key}>
+            <li key={key} onClick={() => openBlock(key)}>
+              {key}
+            </li>
+
+            {selectedKey === key && (
+              <ul>
+                {filteredReports.response[key].map((item: any) => (
+                  <li key={item.id}>{item.sheetName}</li>
+                ))}
+              </ul>
+            )}
+          </div>
         ))
       ) : (
-        <>lox</>
+        <>almaz krasava</>
       )}
     </div>
   );
